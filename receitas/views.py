7f54ef88b7eaddafb9a5df25891f98c18cab29c5
aleprogr45 <aleprogramacao45@gmail.com>
2025-11-27 21:43:36 -0300

    Aula20

--- receitas/views.py
+++ receitas/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 # Create your views here.
 
 a = "Senai"
 
 def home(request):
-    return render(request, "home.html", context={'nome':a})
+    return render(request, "page/home.html", context={'nome':a})
+
+def sobre(request):
+    return HttpResponse("Sobre nóis")
+
+def receita(request):
+    return HttpResponse("As receitas")
+

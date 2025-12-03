da39872f189840a60682b354900d8bec34801e7f
aleprogr45 <aleprogramacao45@gmail.com>
2025-12-03 20:58:34 -0300

    aula06

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categoria, Receita
 
 # Register your models here.
+
+#Forma 1: Usando decorador
+@admin.register(Receita)
+class ReceitaAdmin(admin.ModelAdmin):
+    ...
+
+#Forma 2: Registrando manualmente
+class CategoriaAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Categoria, CategoriaAdmin)
--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#Tabela de categoria de receitas
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+#Tabela de receitas
+class Receita(models.Model):
+    #Atributos da classe receita
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField(help_text="Tempo em minutos")
+    rendimento = models.CharField(max_length=100)
+    criada_em = models.DateTimeField(auto_now_add=True)
+    atualizada_em = models.DateTimeField(auto_now=True)
+    foto_receita = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) #models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True) #models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.titulo
